ee539e8d7380211f2e3c8a225a8dcfcd5be1b813
Vincent <duongvv.webmail@gmail.com>
2024-04-15 22:17:08 -0700

    solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(inputInt) {
+    let fibonacciArray = [1, 1]
+    let index = 1
 
+    if(inputInt == 0){
+        return 0
+    }
+    if(inputInt < 0){
+        return "OOPS"
+    }
+
+    while(fibonacciArray.length < inputInt){
+        fibonacciArray[index+1] = fibonacciArray[index] + fibonacciArray[index-1]
+        index++
+    }
+    return fibonacciArray[inputInt-1]
+    // return fibonacciArray
 };
 
 // Do not edit below this line
