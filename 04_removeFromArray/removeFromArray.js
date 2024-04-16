ff4a35cda20e91d7abd5a65d14ab60b90b77830a
Vincent <duongvv.webmail@gmail.com>
2024-04-15 20:13:06 -0700

    solved removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(inputArray, ...intsToRemove) {
+    for (let i = 0; i < intsToRemove.length; i++){
+        var intToRemove = intsToRemove[i]
+        do{
+            var indexToRemove = inputArray.indexOf(intToRemove);
+            if (indexToRemove > -1) {
+                inputArray .splice(indexToRemove, 1)
+            }
+        } while(inputArray.indexOf(intToRemove)!=-1)
+    }
+    return inputArray
+}
 // Do not edit below this line
 module.exports = removeFromArray;
